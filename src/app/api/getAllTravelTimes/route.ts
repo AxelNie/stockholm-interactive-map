b1dd977688696774d146bb9e35f05812eddde330
AxelNie <axel.nie17@gmail.com>
2024-01-04 20:17:07 +0100

    Implemented caching both database-server and server-client

--- src/app/api/getAllTravelTimes/route.ts
+++ src/app/api/getAllTravelTimes/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { connectToDb } from "../../../../db";
+import NodeCache from "node-cache";
+const myCache = new NodeCache({ stdTTL: 3600 });
 
 export const dynamic = "force-dynamic";
 
@@ -9,23 +11,36 @@ export async function GET(request: Request) {
     const { searchParams } = new URL(request.url);
 
     if (searchParams.get("include_wait_time") === "true") {
-      if (searchParams.get("time") === "23") {
-        collectionName = "TravelTimesAvg23";
-      } else {
-        collectionName = "TravelTimesAvg7_8";
-      }
+      collectionName =
+        searchParams.get("time") === "23"
+          ? "TravelTimesAvg23"
+          : "TravelTimesAvg7_8";
     }
 
-    const { client, collection } = await connectToDb(collectionName);
+    // Kontrollera om datat finns i cachen
+    const cachedData = myCache.get(collectionName);
+    if (cachedData) {
+      return new Response(JSON.stringify({ document: cachedData }), {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control": "public, max-age=86400",
+        },
+      });
+    }
 
-    const document = await collection.findOne();
+    // Hämta data från databasen
+    const document = await fetchDataFromDatabase(collectionName);
 
-    client.close();
+    // Spara data i cachen
+    myCache.set(collectionName, document);
 
+    // Skicka svaret
     return new Response(JSON.stringify({ document }), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
+        "Cache-Control": "public, max-age=86400",
       },
     });
   } catch (error: any) {
@@ -37,5 +52,25 @@ export async function GET(request: Request) {
       error: sanitizedError,
       errorMessage: error.message,
     });
+  }
+}
+
+async function fetchDataFromDatabase(collectionName: string) {
+  try {
+    // Anslut till databasen
+    const { client, collection } = await connectToDb(collectionName);
+
+    // Hämta dokumentet
+    const document = await collection.findOne();
+
+    // Stäng databasanslutningen
+    client.close();
+
+    // Returnera det hämtade dokumentet
+    return document;
+  } catch (error) {
+    // Hantera eventuella fel
+    console.error("Error fetching data from database:", error);
+    throw error; // Skicka felet vidare för att hantera det i GET-funktionen
   }
 }
